73b7ce54f1358f208ca7a3124a2b25df97df500a
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-15 17:01:31 +0100

    Add test for withdraw and refactor test file with beforeEach for var

--- src/__test__/bankAccount.spec.js
+++ src/__test__/bankAccount.spec.js
@@ -3,8 +3,22 @@ const BankAccount = require('../bankAccount.js')
 describe('BankAccount', function () {
   let account
 
+  beforeEach(() => {
+    account = new BankAccount();
+  });
+
   test('starts with balance of 0', function () {
-    account = new BankAccount;
     expect(account.balance).toEqual(0);
+  });
+
+  test('allows user to deposit to their account', function () {
+    account.deposit(50);
+    expect(account.balance).toEqual(50);
+  });
+
+  test('allows user to withdraw from their account', function () {
+    account.deposit(100);
+    account.withdraw(50);
+    expect(account.balance).toEqual(50);
   });
 });
--- src/bankAccount.js
+++ src/bankAccount.js
@@ -2,5 +2,14 @@ class BankAccount {
   constructor() {
     this.balance = 0
   }
-}
+
+  deposit = function (amount) {
+    this.balance += amount
+  };
+
+  withdraw = function (amount) {
+    this.balance -= amount
+  };
+
+};
 module.exports = BankAccount
