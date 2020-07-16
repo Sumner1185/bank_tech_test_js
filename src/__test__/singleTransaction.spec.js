599f9485ded7596aad67ef974c12b0778be1f839
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-16 11:14:56 +0100

    add test for debit and balance to be stored

--- src/__test__/singleTransaction.spec.js
+++ src/__test__/singleTransaction.spec.js
@@ -5,13 +5,21 @@ describe('SingleTransaction', function () {
 
   test('should store a date when transaction is made', function () {
     transaction = new SingleTransaction('16/07/2020');
-
     expect(transaction.date).toEqual('16/07/2020')
   });
 
   test('should store a credit amount when transaction is made', function () {
     transaction = new SingleTransaction('date', '100.00');
-
     expect(transaction.credit).toEqual('100.00');
+  });
+
+  test('should store a debit amount when transaction is made', function () {
+    transaction = new SingleTransaction('date', '', '50.00');
+    expect(transaction.debit).toEqual('50.00');
+  });
+
+  test('should store the resulting balance when transaction is made', function () {
+    transaction = new SingleTransaction('date', '', '50.00', '500.00');
+    expect(transaction.balance).toEqual('500.00');
   });
 });
--- src/singleTransaction.js
+++ src/singleTransaction.js
@@ -1,7 +1,9 @@
 class SingleTransaction {
-  constructor(date, credit) {
+  constructor(date, credit, debit, balance) {
     this.date = date;
     this.credit = credit;
+    this.debit = debit;
+    this.balance = balance;
   };
 };
 module.exports = SingleTransaction;
